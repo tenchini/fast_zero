bcae6604e2556afde36725d69d3433507983aca5
tenchini <tenchini.dev@gmail.com>
2025-01-24 17:49:21 -0300

    aula 02

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,7 +8,7 @@ from fast_zero.app import app
 def test_root_deve_retornar_ok_e_ola_mundo():
     client = TestClient(app)  # Arrange (organizar)
 
-    response = client.get("/")  # Act (agir)
+    response = client.get('/')  # Act (agir)
 
     assert response.status_code == HTTPStatus.OK  # Assert (afirmar)
-    assert response.json() == {"message": "Olá mundo!"}  # Assert (afirmar)
+    assert response.json() == {'message': 'Olá Mundo!'}  # Assert (afirmar)
